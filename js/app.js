966a39a4b11e848998333626d8538f465713361c
manjeet <manjeet175@gmail.com>
2018-05-15 15:45:07 -0400

    fixed linting issues - all resolved except prop-types validation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from './store/store';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter } from 'react-router-dom';
-import WelcomePage from './client/welcomePage.js';
+import store from './store/store';
+import WelcomePage from './client/welcomePage';
 
-const App = () => (<div>
-  <Route exact path="/" component={WelcomePage} />
-</div>);
+const App = () => (
+  <div>
+    <Route exact path="/" component={WelcomePage} />
+  </div>);
 
 ReactDOM.render(
   <Provider store={store}>
--- js/client/welcomePage.js
+++ js/client/welcomePage.js
@@ -1,20 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import store from '../store/store';
-import {connect} from 'react-redux';
-import ErrorBoundary from './errorBoundary.js';
-import TestContent from './testContent.js';
-const mapStateToProps = (store) =>{
-	return {testData : store.init}
-}
+import ErrorBoundary from './errorBoundary';
+import TestContent from './testContent';
+
+const mapStateToProps = () => ({ testData: store.init });
 
 
-class  WelcomePage extends React.Component{
-	render = () => {
-		console.log(this.props.testData);
-		return <ErrorBoundary>
-				<TestContent testData = {this.props.testData} />
-			</ErrorBoundary>
-		}
-}
+const WelcomePage = () => (
+  <ErrorBoundary>
+    <TestContent testData={this.props.testData} />
+  </ErrorBoundary>);
 
 export default connect(mapStateToProps)(WelcomePage);
